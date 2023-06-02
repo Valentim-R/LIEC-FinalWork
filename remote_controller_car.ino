d6aedd9a7b4c72aba1269c28f344f860400fd798
Valentim <76414830+Valentim-R@users.noreply.github.com>
2023-06-02 19:40:02 -0300

    Update remote_controller_car.ino

--- remote_controller_car.ino
+++ remote_controller_car.ino
@@ -1,103 +1,161 @@
+#define servo = 13;
+#define rx = 2;
+#define tx = 3;
 #include <SoftwareSerial.h>
+// bibliotecas e variáveis globais
 #include <Servo.h>
-#define rx = 2
-#define tx = 3
 Servo esqdir;
-SoftwareSerial bluetooth(2, 3);  // RX, TX
-int joystick, pos, dc = 100;
-#define AIB 7  // Direção motor A
-#define AIA 9  // PWM A
-
-
-void setup() {
-  pinMode(AIB, OUTPUT);
-  pinMode(AIA, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(9, OUTPUT);
-  esqdir.attach(13);
-  bluetooth.begin(9600);
-  Serial.begin(9600);
-  for (pos = 200; pos >= 60; pos -= 1) {
-    // o servo volta à posição inicial
-    esqdir.write(pos);
-    delay(2);
-  }
-  for (pos = 0; pos <= 60; pos += 1) {
-    // o servo volta à posição inicial
-    esqdir.write(pos);
-    delay(2);
-  }
+Servo atk;
+Servo def;
+SoftwareSerial bluetooth(2, 3); // RX, TX
+// variáveis
+int joystick, pos, motor;
+int button, cont = 0, b;
+void setup()
+{
+    esqdir.attach(13);
+    atk.attach(12);
+    // inicializações
+    def.attach(11);
+    bluetooth.begin(9600);
+    Serial.begin(9600);
 }
-
-void loop() {
-  if (bluetooth.available()) {
-    joystick = bluetooth.read();
-    if (joystick == 'D') {
-      //se variável bluetooth receber esta letra
-      for (pos = 60; pos <= 85; pos += 1) {
-        // o servo se movimenta para estas coordenadas
-        esqdir.write(pos);
-        delay(2);
-      }
-      dc = dc * 2.55;  // Duty Cycle %
-      analogWrite(AIA, 255 - dc);
-      digitalWrite(AIB, HIGH);
+void loop()
+{
+    if (bluetooth.available())
+    {
+        // variável “button” recebe a informação da porta 7 (botão).
+        button = digitalRead(7);
+        // variável que recebe uma leitura bluetooth
+        joystick = bluetooth.read();
+        // outra variável que recebe a mesma leitura
+        motor = bluetooth.read();
+        if (joystick == 'E')
+        {
+            // se variável bluetooth receber esta letra
+            for (pos = 55; pos <= 110; pos += 1)
+            {
+                // o servo se movimenta para estas coordenadas
+                esqdir.write(pos);
+                delay(20);
+            }
+            digitalWrite(6, HIGH);
+        }
+        if (joystick == 'e')
+        {
+            for (pos = 110; pos >= 55; pos -= 1)
+            {
+                // o servo volta à posição inicial
+                esqdir.write(pos);
+                delay(20);
+            }
+            digitalWrite(6, LOW);
+        }
+        if (joystick == 'D')
+        {
+            // se variável bluetooth receber esta letra
+            for (pos = 55; pos >= 0; pos -= 1)
+            {
+                // o servo se movimenta para estas coordenadas
+                esqdir.write(pos);
+                delay(20);
+            }
+            digitalWrite(6, HIGH);
+        }
+        if (joystick == 'd')
+        {
+            for (pos = 0; pos <= 55; pos += 1)
+            {
+                // o servo volta à posição inicial
+                esqdir.write(pos);
+                delay(20);
+            }
+            digitalWrite(6, HIGH);
+        }
+        if (motor == 'F')
+        {
+            // se variável bluetooth receber esta letra
+            digitalWrite(6, HIGH);
+            // liga-se o motor
+        }
+        if (motor == 'f')
+            digitalWrite(6, LOW);
+        if (joystick == 'A')
+        {
+            // se variável bluetooth receber esta letra
+            for (pos = 0; pos <= 90; pos += 1)
+            {
+                // o servo se movimenta para estas coordenadas
+                atk.write(pos);
+            }
+            for (pos = 90; pos >= 0; pos -= 1)
+            {
+                // o servo volta à posição inicial
+                atk.write(pos);
+                delay(15);
+            }
+        }
+        if (joystick == 'B')
+        {
+            // se variável bluetooth receber esta letra
+            for (pos = 0; pos <= 90; pos += 1)
+            {
+                // o servo se movimenta para estas coordenadas
+                def.write(pos);
+            }
+            for (pos = 90; pos >= 0; pos -= 1)
+            {
+                // o servo volta à posição inicial
+                def.write(pos);
+                delay(15);
+            }
+        }
+        // verifica se o botão foi pressionado.
+        if (button == HIGH)
+        {
+            if (b == 0)
+            {
+                // caso tenha sido pressionado a variável “cont”(contador)
+                adiciona um.cont++;
+            }
+            b = 1;
+        }
+        if (button == LOW)
+        {
+            b = 0;
+        }
+        // caso o contador esteja em zero o LED estará verde.
+        if (cont == 0)
+        {
+            digitalWrite(8, HIGH);
+            digitalWrite(9, LOW);
+            digitalWrite(10, LOW);
+        }
+        // caso o contador esteja em um o LED estará azul.
+        if (cont == 1)
+        {
+            digitalWrite(8, LOW);
+            digitalWrite(9, HIGH);
+            digitalWrite(10, LOW);
+        }
+        // caso o contador esteja em dois o LED estará vermelho.
+        if (cont == 2)
+        {
+            digitalWrite(8, LOW);
+            digitalWrite(9, LOW);
+            digitalWrite(10, HIGH);
+        }
+        // caso o contador esteja em três o LED irá desligar.
+        if (cont == 3)
+        { // LED desliga
+            digitalWrite(8, LOW);
+            digitalWrite(9, LOW);
+            digitalWrite(10, LOW);
+        }
+        // caso o contador passe de 3 ele reseta.
+        if (cont > 3)
+        {
+            cont = 0;
+        }
     }
-
-    if (joystick == 'd') {
-      digitalWrite(6, LOW);
-      for (pos = 110; pos >= 60; pos -= 1) {
-        // o servo volta à posição inicial
-        esqdir.write(pos);
-        delay(2);
-      }
-      analogWrite(AIA, 0);
-      digitalWrite(AIB, LOW);
-    }
-
-    if (joystick == 'E') {
-      //se variável bluetooth receber esta letra
-      for (pos = 60; pos >= 25; pos -= 1) {
-        // o servo se movimenta para estas coordenadas
-        esqdir.write(pos);
-        delay(2);
-      }
-      dc = dc * 2.55;  // Duty Cycle %
-      analogWrite(AIA, 255 - dc);
-      digitalWrite(AIB, HIGH);
-    }
-    if (joystick == 'e') {
-      for (pos = 0; pos <= 60; pos += 1) {
-        // o servo volta à posição inicial
-        esqdir.write(pos);
-        delay(2);
-      }
-      analogWrite(AIA, 0);
-      digitalWrite(AIB, LOW);
-    }
-    if (joystick == 'F') {
-      dc = dc * 2.55;  // Duty Cycle %
-      analogWrite(AIA, 255 - dc);
-      digitalWrite(AIB, HIGH);
-    }
-    if (joystick == 'f') {
-      analogWrite(AIA, 0);
-      digitalWrite(AIB, LOW);
-
-    }
-
-    if (joystick == 'H') {
-      dc = dc * 2.55;  // Duty Cycle %
-      analogWrite(AIA, 255);
-      digitalWrite(AIB, LOW);
-
-    }
-    if (joystick == 'h') {
-      analogWrite(AIA, 0);
-      digitalWrite(AIB, LOW);
-
-    }
-  }
 }
-
